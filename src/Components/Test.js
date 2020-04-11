a76a901038d0ca4f0202f601a358a63a920fa88b
Romi Abdul Yusuf <romi161199@gmail.com>
2020-04-11 14:48:58 +0700

    functional components

--- src/Components/Test.js
+++ src/Components/Test.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 
-export default class Test extends Component {
-    render() {
-        const {list} = this.props
-        const newList = list.map(res=> {
-            return(
-                <div key={res.id}>
+const Test = (props) => {
+    const { list } = props
+    const newList = list.map(res => {
+        return (
+            <div key={res.id}>
                 <div> my name is {res.name}</div>
                 <div>my age is {res.age}</div>
-                </div>
-            )
-        })
-        return (
-            <div>
-                {newList}
-           </div>
+            </div>
         )
-    }
+    })
+    return (
+        <div>
+            {newList}
+        </div>
+    )
 }
+
+export default Test;
